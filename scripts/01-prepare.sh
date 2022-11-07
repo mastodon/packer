eea0ab2075c517409c2592f39bddb620469414f6
Jes Olson <jolson@digitalocean.com>
2022-11-07 14:14:50 -0800

    Bump versions of several components, move to focal
    ruby -> 3.0.3
    mastodon -> 3.5.3
    node -> 16
    ubuntu -> 20.04

--- scripts/01-prepare.sh
+++ scripts/01-prepare.sh
@@ -5,11 +5,18 @@ export DEBIAN_FRONTEND=noninteractive
 cloud-init status --wait \
   && apt -qqy update \
   && apt -qqy -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold' full-upgrade \
-  && apt -qqy install fail2ban iptables-persistent \
-  && curl -sL https://deb.nodesource.com/setup_12.x | bash - \
-  && curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add - \
-  && echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list \
+  && apt -qqy install fail2ban iptables-persistent wget gnupg apt-transport-https lsb-release ca-certificates \
+  && curl -sL https://deb.nodesource.com/setup_16.x | bash - \
+  && wget -O /usr/share/keyrings/postgresql.asc https://www.postgresql.org/media/keys/ACCC4CF8.asc \
+  && echo "deb [signed-by=/usr/share/keyrings/postgresql.asc] http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/postgresql.list \
   && apt -qqy update \
-  && apt -qqy install imagemagick ffmpeg libpq-dev libxml2-dev libxslt1-dev file git-core g++ libprotobuf-dev protobuf-compiler pkg-config nodejs gcc autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm5 libgdbm-dev nginx redis-server redis-tools postgresql postgresql-contrib certbot python-certbot-nginx yarn libidn11-dev libicu-dev libjemalloc-dev \
+  && apt -qqy install imagemagick ffmpeg libpq-dev libxml2-dev libxslt1-dev file git-core \
+    g++ libprotobuf-dev protobuf-compiler pkg-config nodejs gcc autoconf \
+    bison build-essential libssl-dev libyaml-dev libreadline6-dev \
+    zlib1g-dev libncurses5-dev libffi-dev libgdbm-dev \
+    nginx redis-server redis-tools postgresql postgresql-contrib \
+    certbot python3-certbot-nginx libidn11-dev libicu-dev libjemalloc-dev \
+  && corepack enable \
+  && yarn set version stable \
   && adduser --disabled-login --gecos '' mastodon \
   && sudo -u postgres psql -c "CREATE USER mastodon CREATEDB;"
