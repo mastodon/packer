1a3ccd2b1f0da44808454818df03b1fa09725bde
Jes Olson <jolson@digitalocean.com>
2022-11-08 13:24:52 -0800

    Fix small nginx/certbot bug

--- files/opt/mastodon/setup.sh
+++ files/opt/mastodon/setup.sh
@@ -4,12 +4,13 @@ echo "Booting Mastodon's first-time setup wizard..." &&
   su - mastodon -c "cd /home/mastodon/live && RAILS_ENV=production /home/mastodon/.rbenv/shims/bundle exec rake digitalocean:setup" &&
   export $(grep '^LOCAL_DOMAIN=' /home/mastodon/live/.env.production | xargs) &&
   echo "Launching Let's Encrypt utility to obtain SSL certificate..." &&
+  systemctl stop nginx &&
   certbot certonly --standalone --agree-tos -d $LOCAL_DOMAIN &&
   cp /home/mastodon/live/dist/nginx.conf /etc/nginx/sites-available/mastodon &&
   sed -i -- "s/example.com/$LOCAL_DOMAIN/g" /etc/nginx/sites-available/mastodon &&
   ln -sfn /etc/nginx/sites-available/mastodon /etc/nginx/sites-enabled/mastodon &&
   sed -i -- "s/  # ssl_certificate/  ssl_certificate/" /etc/nginx/sites-available/mastodon &&
-  systemctl restart nginx &&
+  systemctl start nginx &&
   systemctl enable mastodon-web && systemctl start mastodon-web &&
   systemctl enable mastodon-streaming && systemctl start mastodon-streaming &&
   systemctl enable mastodon-sidekiq && systemctl start mastodon-sidekiq &&
