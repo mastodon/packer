1ef540adb3f9c5b49b9b5ed554c21c25d00c6a08
Michael Stanclift <mx@vmstan.com>
2023-10-11 13:45:36 -0500

    sort installs, add nginx node yarn and redis repos

--- scripts/01-prepare.sh
+++ scripts/01-prepare.sh
@@ -5,18 +5,56 @@ export DEBIAN_FRONTEND=noninteractive
 cloud-init status --wait \
   && apt -qqy update \
   && apt -qqy -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold' full-upgrade \
-  && apt -qqy install fail2ban iptables-persistent wget gnupg apt-transport-https lsb-release ca-certificates \
-  && curl -sS -o - https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor | tee /usr/share/keyrings/nodesource.gpg >/dev/null; \
-  && curl -sS -o - https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor | tee /usr/share/keyrings/yarnkey.gpg >/dev/null; \
+  && apt -qqy install \
+    ca-certificates \
+    curl \
+    fail2ban \
+    ffmpeg \
+    file \
+    g++ \
+    gcc \
+    git \
+    imagemagick \
+    iptables-persistent \
+    libgdbm-dev \
+    libgmp-dev \
+    libicu-dev \
+    libicu72 \
+    libidn-dev \
+    libidn12 \
+    libjemalloc2 \
+    libpq-dev \
+    libpq5 \
+    libreadline8 \
+    libssl-dev \
+    libssl3 \
+    libyaml-0-2 \
+    make \
+    patchelf \
+    procps \
+    shared-mime-info \
+    tini \
+    tzdata \
+    zlib1g-dev \
+  && curl -sS -o - https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor | tee /usr/share/keyrings/nodesource.gpg >/dev/null \
+  && curl -sS -o - https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor | tee /usr/share/keyrings/yarnkey.gpg >/dev/null \
   && curl -sS -o /usr/share/keyrings/postgresql.asc https://www.postgresql.org/media/keys/ACCC4CF8.asc \
-  && echo "deb [signed-by=/usr/share/keyrings/postgresql.asc] http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/postgresql.list \
+  && curl -sS -o - https://packages.redis.io/gpg | gpg --dearmor | tee /usr/share/keyrings/redis.gpg >/dev/null \
+  && curl -sS -o - https://nginx.org/keys/nginx_signing.key | gpg --dearmor | tee /usr/share/keyrings/nginx.gpg >/dev/null \
+  && echo "deb [signed-by=/usr/share/keyrings/nginx.gpg] http://nginx.org/packages/debian/ $(lsb_release -cs) nginx" | tee /etc/apt/sources.list.d/nginx.list \
+  && echo "deb [signed-by=/usr/share/keyrings/yarnkey.gpg] https://dl.yarnpkg.com/debian stable main" | tee /etc/apt/sources.list.d/yarn.list \
+  && echo "deb [signed-by=/usr/share/keyrings/postgresql.asc] http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" | tee /etc/apt/sources.list.d/postgresql.list \
+  && echo "deb [signed-by=/usr/share/keyrings/redis.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/redis.list \
+  && echo "deb [signed-by=/usr/share/keyrings/nginx.gpg] http://nginx.org/packages/debian/ $(lsb_release -cs) nginx" | tee -a /etc/apt/sources.list.d/nginx.list \
   && apt -qqy update \
-  && apt -qqy install imagemagick ffmpeg libpq-dev libxml2-dev libxslt1-dev file git-core \
-    g++ libprotobuf-dev protobuf-compiler pkg-config nodejs gcc autoconf \
-    bison build-essential libssl-dev libyaml-dev libreadline6-dev \
-    zlib1g-dev libncurses5-dev libffi-dev libgdbm-dev \
-    nginx redis-server redis-tools postgresql postgresql-contrib \
-    certbot python3-certbot-nginx libidn11-dev libicu-dev libjemalloc-dev \
+  && apt -qqy install \
+    certbot \
+    nginx \
+    postgresql \
+    postgresql-contrib \
+    python3-certbot-nginx \
+    redis-server \
+    redis-tools \
   && corepack enable \
   && yarn set version stable \
   && adduser --disabled-login --gecos '' mastodon \
