18f7ef7115def158ee91d05d631774f55a2a8d3f
Michael Stanclift <mx@vmstan.com>
2023-10-11 13:24:38 -0500

    add node repos and change to curl

--- scripts/01-prepare.sh
+++ scripts/01-prepare.sh
@@ -6,8 +6,9 @@ cloud-init status --wait \
   && apt -qqy update \
   && apt -qqy -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold' full-upgrade \
   && apt -qqy install fail2ban iptables-persistent wget gnupg apt-transport-https lsb-release ca-certificates \
-  && curl -sL https://deb.nodesource.com/setup_16.x | bash - \
-  && wget -O /usr/share/keyrings/postgresql.asc https://www.postgresql.org/media/keys/ACCC4CF8.asc \
+  && curl -sS -o - https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor | tee /usr/share/keyrings/nodesource.gpg >/dev/null; \
+  && curl -sS -o - https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor | tee /usr/share/keyrings/yarnkey.gpg >/dev/null; \
+  && curl -sS -o /usr/share/keyrings/postgresql.asc https://www.postgresql.org/media/keys/ACCC4CF8.asc \
   && echo "deb [signed-by=/usr/share/keyrings/postgresql.asc] http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/postgresql.list \
   && apt -qqy update \
   && apt -qqy install imagemagick ffmpeg libpq-dev libxml2-dev libxslt1-dev file git-core \
